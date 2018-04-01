118dafcba4c4c11ed8965e8e675c6a2f67570dcb
aBarichello <arturbarichello@hotmail.com>
2018-03-31 21:11:47 -0300

    Prototype keys

--- game/scripts/Main.gd
+++ game/scripts/Main.gd
@@ -1,4 +1,12 @@
 extends Node
 
 func _ready():
-    pass
+    randomize()
+    if randi() % 10 == 0:
+        $Title.text = "ENIGMA"
+        $SecretTimer.start()
+    else:
+        $Title.text = "GAMINE"
+
+func _on_SecretTimer_timeout():
+    $Title.text = "GAMINE"
