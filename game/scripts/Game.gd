2d83d655b330f429327d7ff40e352863103b3bfe
aBarichello <arturbarichello@hotmail.com>
2018-04-01 01:33:11 -0300

    Visual feedback

--- game/scripts/Game.gd
+++ game/scripts/Game.gd
@@ -11,8 +11,7 @@ var lower_row = []
 var upper_select = []
 var lower_select = []
 
-var upper_index = 0
-var lower_index = 0
+var level = 0
 var selecting_upper = true
 
 func _ready():
@@ -33,6 +32,9 @@ func _ready():
     map_row(upper_select, UPPER_SELECT)
     map_row(lower_select, LOWER_SELECT)
 
+    # Starting positions
+    $">".rect_position = Vector2($Answers.rect_position.x - $">".rect_size.x, $Answers.rect_position.y)
+
 func _input(event):
     var Upper = get_node("Answers/UpperSelect")
     var Lower = get_node("Answers/LowerSelect")
@@ -49,8 +51,18 @@ func _input(event):
             Lower.move_child(Lower.get_child(Lower.get_child_count() - 1), 0)
     if event.is_action_pressed("ui_up"):
         selecting_upper = true
+        $">".rect_position.y = $Answers/UpperSelect.rect_global_position.y
     if event.is_action_pressed("ui_down"):
         selecting_upper = false
+        $">".rect_position.y = $Answers/LowerSelect.rect_global_position.y
+    if event.is_action_pressed("ui_accept"):
+        check_selection()
+
+func _process(delta):
+    # Align V with current level
+    var enigma_pos = $Enigmas/UpperRow.rect_global_position
+    var move_offset = enigma_pos.x / 14
+    $V.rect_global_position = Vector2(enigma_pos.x + move_offset * level, enigma_pos.y - $V.rect_size.y)
 
 func fill_row(row):
     for i in range(0, 7):
@@ -83,10 +95,32 @@ func shuffle_array(arr):
             shuffled.push_front(x)
     return shuffled
 
+# Map row of random numbers to pieces/text
 func map_row(row, target_path):
     for i in range(0, row.size()):
         var button = Button.new()
         var id = str(row[i])
         button.name = id
         button.text = id
+        button.rect_min_size = Vector2(50, 50)
+        button.rect_pivot_offset = button.rect_min_size / 2
         get_node(target_path).add_child(button)
+
+# Check the center selection against the
+func check_selection():
+    var up_ok = int($Answers/UpperSelect.get_child(4).name) == upper_row[level]
+    var down_ok = int($Answers/LowerSelect.get_child(4).name) == lower_row[level]
+    if up_ok and down_ok:
+        level += 1
+        print(level)
+    else:
+        print("WRONG")
+
+# --- Timers ---
+
+func _on_Timer_timeout():
+    var marker = $">"
+    if marker.text == ">":
+        marker.text = ""
+    else:
+        marker.text = ">"
