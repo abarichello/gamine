47ae19e6f0d263c5aee09fe9e5fa53af0c7f264f
aBARICHELLO <arturbarichello@hotmail.com>
2018-04-03 00:35:02 -0300

    Started control nodes

--- game/scripts/Game.gd
+++ game/scripts/Game.gd
@@ -1,10 +1,10 @@
 extends Node
 
 const VARIATIONS = 20
-const UPPER_ROW = "Enigmas/UpperRow"
-const LOWER_ROW = "Enigmas/LowerRow"
-const UPPER_SELECT = "Answers/UpperSelect"
-const LOWER_SELECT = "Answers/LowerSelect"
+const UPPER_ROW = "2/Enigmas/UpperRow"
+const LOWER_ROW = "2/Enigmas/LowerRow"
+const UPPER_SELECT = "3/Answers/UpperSelect"
+const LOWER_SELECT = "3/Answers/LowerSelect"
 
 var upper_row = []
 var lower_row = []
@@ -16,52 +16,50 @@ var selecting_upper = true
 
 func _ready():
     fill_row(upper_row)
-    print(upper_row)
     fill_row(lower_row)
-    print(lower_row)
-
-    print("---")
 
     upper_select = shuffle_array(fill_select(upper_row, upper_select))
-    print(upper_select)
     lower_select = shuffle_array(fill_select(lower_row, lower_select))
-    print(lower_select)
 
-    map_row(upper_row, UPPER_ROW)
-    map_row(lower_row, LOWER_ROW)
-    map_row(upper_select, UPPER_SELECT)
-    map_row(lower_select, LOWER_SELECT)
+    map_row(upper_row, UPPER_ROW, 100)
+    map_row(lower_row, LOWER_ROW, 100)
+    map_row(upper_select, UPPER_SELECT, 100)
+    map_row(lower_select, LOWER_SELECT, 100)
 
     # Starting positions
-    $">".rect_position = Vector2($Answers.rect_position.x - $">".rect_size.x, $Answers.rect_position.y)
+    $"3/>".rect_position = Vector2($"3/Answers".rect_position.x - $"3/>".rect_size.x, $"3/Answers".rect_position.y)
 
 func _input(event):
-    var Upper = get_node("Answers/UpperSelect")
-    var Lower = get_node("Answers/LowerSelect")
+    var Upper = get_node(UPPER_SELECT)
+    var Lower = get_node(LOWER_SELECT)
 
     if event.is_action_pressed("ui_left"):
         if selecting_upper:
             Upper.move_child(Upper.get_child(0), Upper.get_child_count() - 1)
         else:
             Lower.move_child(Lower.get_child(0), Lower.get_child_count() - 1)
+
     if event.is_action_pressed("ui_right"):
         if selecting_upper:
             Upper.move_child(Upper.get_child(Upper.get_child_count() - 1), 0)
         else:
             Lower.move_child(Lower.get_child(Lower.get_child_count() - 1), 0)
+
     if event.is_action_pressed("ui_up"):
         selecting_upper = true
-        $">".rect_position.y = $Answers/UpperSelect.rect_global_position.y
+        $"3/>".rect_position.y = Upper.rect_global_position.y
+
     if event.is_action_pressed("ui_down"):
         selecting_upper = false
-        $">".rect_position.y = $Answers/LowerSelect.rect_global_position.y
+        $"3/>".rect_position.y = Lower.rect_global_position.y
+
     if event.is_action_pressed("ui_accept"):
         check_selection()
 
 func _process(delta):
     # Align V with current level
-    var current = $Enigmas/UpperRow.get_child(level).rect_global_position
-    $V.rect_global_position = Vector2(current.x, current.y - $V.rect_size.y)
+    var current = $"2/Enigmas/UpperRow".get_child(level).rect_global_position
+    $"2/V".rect_global_position = Vector2(current.x, current.y - $"2/V".rect_size.y)
 
 func fill_row(row):
     for i in range(0, 7):
@@ -83,6 +81,7 @@ func fill_select(row_in, row_out):
     return row_out
 
 # Why isn't this built in?
+# Placeholder
 func shuffle_array(arr):
     var shuffled = []
     for i in range(arr.size()):
@@ -95,20 +94,20 @@ func shuffle_array(arr):
     return shuffled
 
 # Map row of random numbers to pieces/text
-func map_row(row, target_path):
+func map_row(row, target_path, size):
     for i in range(0, row.size()):
         var button = Button.new()
         var id = str(row[i])
         button.name = id
         button.text = id
-        button.rect_min_size = Vector2(50, 50)
+        button.rect_min_size = Vector2(size, size)
         button.rect_pivot_offset = button.rect_min_size / 2
         get_node(target_path).add_child(button)
 
 # Check the center selection against the current level
 func check_selection():
-    var up_ok = int($Answers/UpperSelect.get_child(4).name) == upper_row[level]
-    var down_ok = int($Answers/LowerSelect.get_child(4).name) == lower_row[level]
+    var up_ok = int(get_node(UPPER_SELECT).get_child(4).name) == upper_row[level]
+    var down_ok = int(get_node(LOWER_SELECT).get_child(4).name) == lower_row[level]
     if up_ok and down_ok:
         level += 1
         print(level)
@@ -118,7 +117,7 @@ func check_selection():
 # --- Timers ---
 
 func _on_Timer_timeout():
-    var marker = $">"
+    var marker = $"3/>"
     if marker.text == ">":
         marker.text = ""
     else:
--- game/scripts/Main.gd
+++ game/scripts/Main.gd
@@ -1,6 +1,12 @@
 extends Node
 
+var screensize
+
 func _ready():
+    screensize = get_node("/root").get_viewport().get_visible_rect().size
+    $Title.rect_position.x = screensize.x / 2 - $Title.rect_size.x / 2
+    $Title.rect_position.y = screensize.y / 4 - $Title.rect_size.y / 2
+
     randomize()
     if randi() % 10 == 0:
         $Title.text = "ENIGMA"
