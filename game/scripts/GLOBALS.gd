163ab50f6227f3baf1b04db401b222e669c4a641
aBarichello <arturbarichello@hotmail.com>
2018-04-22 00:59:01 -0300

    Closes #1

---
+++ game/scripts/GLOBALS.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Columns
+const COLUMNS_ROW = 7
+const COLUMNS_SELECT = 9
+
+# Magic numbers
+const VARIATIONS = 20
+const PIECE_VARIATIONS = 20
+const TIME_AVAILABLE = 15
+
+# Paths
+const UPPER_ROW = "2/Enigmas/UpperRow"
+const LOWER_ROW = "2/Enigmas/LowerRow"
+const UPPER_SELECT = "3/Answers/UpperSelect"
+const LOWER_SELECT = "3/Answers/LowerSelect"
