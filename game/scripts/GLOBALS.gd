ef227dd0eb21f8046bcad1abd3e2afc3418983d1
aBARICHELLO <arturbarichello@hotmail.com>
2018-05-18 13:51:42 -0300

    Piece highlighting functions

--- game/scripts/GLOBALS.gd
+++ game/scripts/GLOBALS.gd
@@ -2,11 +2,14 @@ extends Node
 
 var screensize = OS.get_screen_size()
 
+# Colors
 const FERGUS_THEME = [Color(1, 0, 0), Color()]
 const WYATT_THEME = [Color(0, 1, 0), Color()]
 const STRASSE_THEME = [Color(1, 0.56, 0), Color()]
+const HIGHLIGHT = Color(0.1, 0.1, 0.1)
 
-var selected_theme = 0
+var theme_index = 0
+var CURRENT_THEME = self.THEMES[self.theme_index][0]
 const THEMES = [FERGUS_THEME, WYATT_THEME, STRASSE_THEME]
 
 # Columns
