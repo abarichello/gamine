17e21f9c9512e14ad93dea56d0cd97a40e80a3a0
aBARICHELLO <arturbarichello@hotmail.com>
2018-06-20 23:11:02 -0300

    Implement simple leaderboard functions
    TODO handle missing params

--- server/src/handlers/leaderboard.ts
+++ server/src/handlers/leaderboard.ts
@@ -4,8 +4,6 @@ import pg from '../setup/database'
 
 export async function addEntry(req: Request, res: Response, next: NextFunction) {
     const { game, nickname, score } = req.body
-    console.log(`GAME: ${game}\nSCORE: ${score}`)
-
     const entry = await pg
         .from(game)
         .insert({ nickname, score })
@@ -13,16 +11,19 @@ export async function addEntry(req: Request, res: Response, next: NextFunction) 
 }
 
 export async function getEntry(req: Request, res: Response, next: NextFunction) {
-    return res.status(200).end()
+    const { game, nickname } = req.query
+    const userEntries = await pg
+        .from(game)
+        .select('score', 'added')
+        .where({ nickname })
+    return res.status(200).json({ userEntries })
 }
 
 export async function getTopEntries(req: Request, res: Response, next: NextFunction) {
     const { game } = req.query
-
     const topEntries = await pg
         .from(game)
         .select('nickname', 'score')
         .count('score')
-
-    return res.status(200).end()
+    return res.status(200).json({ topEntries })
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 require('dotenv-safe').load()
 require('source-map-support').install() // Map generated JS source back to TS
 import * as https from 'https'
+import * as http from 'http'
 import { createServerInstance } from './setup/express'
 import { readFileSync } from 'fs'
 
@@ -14,6 +15,12 @@ const credentials = {
 const app = createServerInstance()
 const port = Number(process.env.PORT) || 3000
 
-https.createServer(credentials, app).listen(port, function() {
-    console.log(`Express server listening on port ${port}`)
-})
+if (process.env.NODE_ENV === 'development') {
+    http.createServer(app).listen(port, function() {
+        console.log(`Express HTTP server listening on port ${port}`)
+    })
+} else {
+    https.createServer(credentials, app).listen(port, function() {
+        console.log(`Express HTTPS server listening on port ${port}`)
+    })
+}
