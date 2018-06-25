c65dc37dfad0aadf12769e18427bc2a2b1c2f28d
aBARICHELLO <arturbarichello@hotmail.com>
2018-06-25 02:58:53 -0300

    Testing Network script
    Testing Godot's HTTPRequest node

--- game/scripts/GLOBALS.gd
+++ game/scripts/GLOBALS.gd
@@ -1,8 +1,8 @@
 extends Node
 
+# System variables
 var SCREENSIZE = OS.get_screen_size()
-
-const ROOT_URL = "http://localhost:3000"
+var ROOT_URL = OS.get_environment("ROOT_URL")
 
 # Colors
 const FERGUS_THEME = Color(1, 0, 0)
--- game/scripts/Network.gd
+++ game/scripts/Network.gd
@@ -2,11 +2,32 @@ extends HTTPRequest
 
 onready var global = get_node("/root/Main/GLOBALS")
 const JSON_HEADER = ["Content-Type: application/json"]
+const LOCALHOST = "http://localhost:3000"
+
+var debug = true
 
 func _ready():
-    var test = {"game": "GAMINE", "value": 8}
-    make_post("/leaderboard/add", test)
+    if debug:
+        test()
 
-func make_post(url, contents):
+func post(url, contents):
     var body = JSON.print(contents)
-    self.request(global.ROOT_URL + url, JSON_HEADER, true, HTTPClient.METHOD_POST, body)
+    if debug:
+        self.request(LOCALHOST + url, JSON_HEADER, false, HTTPClient.METHOD_POST, body)
+    else:
+        self.request(global.ROOT_URL + url, JSON_HEADER, true, HTTPClient.METHOD_POST, body)
+
+func get(url):
+    if debug:
+        self.request(LOCALHOST + url, JSON_HEADER, false, HTTPClient.METHOD_GET)
+
+# DEBUG
+func test():
+    #var test = {"game": "gamine", "nickname": "Barichello", "score": 8}
+    #post("/leaderboard", test)
+    get("/leaderboard?game=gamine&nickname=Barichello")
+
+func _on_Network_request_completed(result, response_code, headers, body):
+    var json = JSON.parse(body.get_string_from_utf8())
+    print(response_code)
+    print(json.result)
