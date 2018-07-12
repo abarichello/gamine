a41ffc54a6f6c6e374f302e7875f54cf1b6f6407
BARICHELLO <arturbarichello@hotmail.com>
2018-07-12 02:32:31 -0300

    Add Leaderboard scene and script

--- game/scripts/Network.gd
+++ game/scripts/Network.gd
@@ -4,11 +4,11 @@ onready var global = get_node("/root/Main/GLOBALS")
 const JSON_HEADER = ["Content-Type: application/json"]
 const LOCALHOST = "http://localhost:3000"
 
-var debug = true
+var debug = bool(OS.get_environment("GAMINE_DEBUG"))
 
 func post(url, contents):
     var body = JSON.print(contents)
-    if debug:
-        self.request(LOCALHOST + url, JSON_HEADER, false, HTTPClient.METHOD_POST, body)
-    else:
+    if not debug:
         self.request(global.ROOT_URL + url, JSON_HEADER, true, HTTPClient.METHOD_POST, body)
+    else:
+        self.request(LOCALHOST + url, JSON_HEADER, false, HTTPClient.METHOD_POST, body)
