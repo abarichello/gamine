84bfbfb9d76ece6f63cc96127a10f686d6b8deef
BARICHELLO <arturbarichello@hotmail.com>
2018-07-14 02:04:50 -0300

    Implement Leaderboard rank generation
     - Fixed some variable naming and ordering in multiple files for consistency.\n - Implemented add_rank Leaderboard method for filling RankStack, created a Scene RankLabel for this purpose.\n - Added font used in RankLabel's scene font preset

--- game/scripts/Leaderboard.gd
+++ game/scripts/Leaderboard.gd
@@ -1,15 +1,18 @@
 extends Control
 
+export (PackedScene) var RankLabel
+
 const JSON_HEADER = ["Content-Type: application/json"]
 const LOCALHOST = "http://localhost:3000/leaderboard/top?game=gamine&type=round&limit=10"
-
 var debug = bool(OS.get_environment("GAMINE_DEBUG"))
-var TEST_JSON = JSON.parse({"topEntries": [
-    { "nickname": testnickname, "score": 100 },
-    { "nickname": TEST, "score": 200 }]})
+var rank_counter = 1
+
+# DEBUG
+var content
+var test_json
 
 func _ready():
-    request_leaderboard()
+    self.popup()
 
 func request_leaderboard():
     if not debug:
@@ -18,8 +21,38 @@ func request_leaderboard():
     else:
         $Network.request(LOCALHOST, JSON_HEADER, false, HTTPClient.METHOD_GET)
 
+func add_rank():
+    var label = RankLabel.instance()
+    if rank_counter < 10:  # Number padding
+        label.text = "   " + str(rank_counter)
+    else:
+        label.text = str(rank_counter)
+    $BackPanel/MainStack/ScoresContainer/RankStack.add_child(label, true)
+    rank_counter += 1
+
+func add_nickname(nickname):
+    pass
+
+func add_score(score):
+    pass
+
+# --- Signals ---
+
+func _on_Leaderboard_about_to_show():
+    rank_counter = 1
+    var file = File.new()
+    file.open("res://test.json", File.READ)
+    content = file.get_as_text()
+    test_json = JSON.parse(content)
+    request_leaderboard()
+
 func _on_Network_request_completed(result, response_code, headers, body):
-    var json = JSON.parse(body.get_string_from_utf8())
-    print(result)
-    print(response_code)
-    print(json.result)
+    # var json = JSON.parse(body.get_string_from_utf8())
+    # TODO: Handle response code
+    var json = test_json
+    for value in json.result.topEntries:
+        var entry = value.values()
+        print(entry)
+        add_rank()
+        add_nickname(entry[0])
+        add_score(entry[1])
--- game/scripts/Network.gd
+++ game/scripts/Network.gd
@@ -1,9 +1,9 @@
 extends HTTPRequest
 
 onready var global = get_node("/root/Main/GLOBALS")
+
 const JSON_HEADER = ["Content-Type: application/json"]
 const LOCALHOST = "http://localhost:3000"
-
 var debug = bool(OS.get_environment("GAMINE_DEBUG"))
 
 func post(url, contents):
