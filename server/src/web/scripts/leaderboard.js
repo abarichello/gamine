ac8f3d94935d63c43e397dfb52a2d195602f8473
BARICHELLO <arturbarichello@hotmail.com>
2018-07-20 00:13:37 -0300

    Add GET Request to leaderboard.js

--- server/src/web/scripts/leaderboard.js
+++ server/src/web/scripts/leaderboard.js
@@ -1,10 +1,23 @@
-function generateLeaderboard() {
+const uri = 'http://localhost:3000'
+
+var xhr = new XMLHttpRequest()
+xhr.open('GET', uri + '/leaderboard/top?game=gamine&type=round&limit=25', true)
+xhr.send()
+xhr.onreadystatechange = generateLeaderboard
+
+function generateLeaderboard(err) {
+    if (xhr.status !== 200) {
+        return
+    }
+
     var table = document.getElementById('leaderboardTable')
-    var newRow = table.insertRow(-1)
-    var cell1 = newRow.insertCell(-1)
-    var cell2 = newRow.insertCell(-1)
-    cell1.innerHTML = 'NEW CELL1'
-    cell2.innerHTML = 'NEW CELL2'
+    for (entry of JSON.parse(xhr.response).topEntries) {
+        var newRow = table.insertRow(-1)
+        var nicknameCell = newRow.insertCell(-1)
+        var scoreCell = newRow.insertCell(-1)
+        nicknameCell.innerHTML = entry.nickname
+        scoreCell.innerHTML = entry.score
+    }
 }
 
 window.onload = generateLeaderboard
