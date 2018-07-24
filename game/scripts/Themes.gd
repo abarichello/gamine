748548851186f4cd2af01ff31da9be786ccb6fdd
BARICHELLO <arturbarichello@hotmail.com>
2018-07-24 01:12:08 -0300

    Add ASCIITheme

--- game/scripts/Themes.gd
+++ game/scripts/Themes.gd
@@ -1,4 +1,10 @@
 extends Popup
 
+onready var global = get_node("/root/Main/GLOBALS")
+
 func _ready():
     self.popup()
+    $Themes/FergusASCII.select_all()
+
+func _on_Themes_about_to_show():
+    self.modulate = global.current_theme
