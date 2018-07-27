e8d6e78898ad21c1a46498958540b7083ab08417
BARICHELLO <arturbarichello@hotmail.com>
2018-07-27 01:22:41 -0300

    Add remaining ASCII art
    Removed old font and added new true monospaced font

--- game/scripts/Themes.gd
+++ game/scripts/Themes.gd
@@ -4,7 +4,6 @@ onready var global = get_node("/root/Main/GLOBALS")
 
 func _ready():
     self.popup()
-    $Themes/FergusASCII.select_all()
 
 func _on_Themes_about_to_show():
     self.modulate = global.current_theme
