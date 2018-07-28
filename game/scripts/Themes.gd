56292ea972affa50ca3405d82adbdc3bc5f0665a
BARICHELLO <arturbarichello@hotmail.com>
2018-07-28 01:03:32 -0300

    Implement quotes

--- game/scripts/Themes.gd
+++ game/scripts/Themes.gd
@@ -1,22 +1,20 @@
 extends Popup
 
-func _ready():
-    pass
-    #self.popup()
-
-func _on_Themes_about_to_show():
-    self.modulate = GLOBAL.current_theme
-    highlight()
-
 # Highlights selected theme and deselects the rest
 func highlight():
     $Themes.get_child(GLOBAL.theme_index).select_all()
     var tmp = GLOBAL.theme_index
+    $Quotes.get_child(tmp).visible = true
     for i in range(2):
         tmp = (tmp + 1) % 3
         $Themes.get_child(tmp).deselect()
+        $Quotes.get_child(tmp).visible = false
 
 # --- Signals ---
+
+func _on_Themes_about_to_show():
+    self.modulate = GLOBAL.current_theme
+    highlight()
 
 func _on_FergusASCII_focus_entered():
     GLOBAL.update_theme(0)
