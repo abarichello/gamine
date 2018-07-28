fbc2847468fac39e82b9ec66c393fb476af5c408
BARICHELLO <arturbarichello@hotmail.com>
2018-07-28 19:27:53 -0300

    Add reused SocialMediaBox

---
+++ game/scripts/SocialMediaBox.gd
@@ -0,0 +1,12 @@
+extends HBoxContainer
+
+# --- Social media buttons ---
+
+func _on_Twitter_pressed():
+    OS.shell_open("https://twitter.com/a_barichello")
+
+func _on_GitHub_pressed():
+    OS.shell_open("https://github.com/abarichello/gamine")
+
+func _on_Telegram_pressed():
+    OS.shell_open("https://t.me/barichello")
