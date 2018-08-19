8ef58ac9bdfcb769c06e05f7b981df47d38d2dd3
BARICHELLO <arturbarichello@hotmail.com>
2018-08-19 00:45:41 -0300

    Add Nickname database functions

--- game/scripts/Main.gd
+++ game/scripts/Main.gd
@@ -9,25 +9,11 @@ func apply_theme():
     self.modulate = GLOBAL.current_theme
 
 func save_data():
-    var save_file = File.new()
-    save_file.open("user://savedata.gamine", File.WRITE)
     var save_nodes = get_node("/root/Main").get_tree().get_nodes_in_group("Serialize")
     for node in save_nodes:
-        node.serialize(save_file)
-    save_file.close()
+        pass
 
 func load_data():
-    var save_file = File.new()
-    if !save_file.file_exists("user://savedata.gamine"):
-        print("No save located")
-        return
-
-    # Save data is an array of save_data dictionaries
-    save_file.open("user://savedata.gamine", File.READ)
     var save_nodes = get_node("/root/Main").get_tree().get_nodes_in_group("Serialize")
-    var save_data = []
-    while !save_file.eof_reached():
-        save_data.append(parse_json(save_file.get_line()))
-
     for node in save_nodes:
-        node.unserialize(save_data)
+        pass
