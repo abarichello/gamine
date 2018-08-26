5e68208f96d7cfcd802be2bb97b061e37a336117
BARICHELLO <arturbarichello@hotmail.com>
2018-08-26 01:59:54 -0300

    Remove dud TextEdit nodes
    Dud nodes are no longer needed as explained in code comments, there's an available property for that (focus_mode); Added more constants in globals.

--- game/scripts/Themes.gd
+++ game/scripts/Themes.gd
@@ -3,6 +3,8 @@ extends Popup
 # Highlights selected theme and deselects the rest
 func highlight():
     $Themes.get_child(GLOBAL.theme_index).select_all()
+
+    # Deselect other themes and hide their quotes
     var tmp = GLOBAL.theme_index
     $Quotes.get_child(tmp).visible = true
     for i in range(2):
@@ -11,22 +13,19 @@ func highlight():
         $Quotes.get_child(tmp).visible = false
 
 # --- Signals ---
-# I am using TextEdit nodes as buttons and their focus_entered signal for buttons presses,
-# since the first TextEdit in the scene gets automatically focused there needs to be a 'dud' ($Dud)
-# node without the connected signal to prevent unwanted triggers.
 
 func _on_Themes_about_to_show():
     self.modulate = GLOBAL.current_theme
     self.highlight()
 
 func _on_FergusASCII_focus_entered():
-    GLOBAL.update_theme(0)
+    GLOBAL.update_theme(GLOBAL.FERGUS_THEME)
     self._on_Themes_about_to_show()
 
 func _on_WyattASCII_focus_entered():
-    GLOBAL.update_theme(1)
+    GLOBAL.update_theme(GLOBAL.WYATT_THEME)
     self._on_Themes_about_to_show()
 
 func _on_StrasseASCII_focus_entered():
-    GLOBAL.update_theme(2)
+    GLOBAL.update_theme(GLOBAL.STRASSE_THEME)
     self._on_Themes_about_to_show()
